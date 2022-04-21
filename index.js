bcd9bef14773a645af95fbe88d25add82400ff25
Micah Bell <micahjoshuabell@gmail.com>
2022-04-21 13:43:21 -0400

    Add createGrid function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //
+
+let userGridNumber = 100;
+const gridContainer = document.querySelector('#gridContainer');
+
+function createGrid (gridNumber) {
+    for (let i = 0; i < gridNumber; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.classList.add('hover');
+        div.classList.toggle('hover');
+        gridContainer.appendChild(div);
+    }
+}
+
+createGrid(userGridNumber);
