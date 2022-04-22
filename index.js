3e6572051715b4943ee252b45283ea0516a8b352
Micah Bell <micahjoshuabell@gmail.com>
2022-04-22 11:12:53 -0400

    Add Grid Flow and EventListeners

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 //
 
-let userGridNumber = 100;
+let userGridNumber = 64;
 const gridContainer = document.querySelector('#gridContainer');
+const resetButton = document.querySelector('#reset');
+
+// Set Grid size
+gridContainer.style.gridTemplateColumns = `repeat(${Math.round(Math.sqrt(userGridNumber))}, 1fr)`;
+
+// Reset the Grid
+
+
 
 function createGrid (gridNumber) {
     for (let i = 0; i < gridNumber; i++) {
@@ -12,5 +20,15 @@ function createGrid (gridNumber) {
         gridContainer.appendChild(div);
     }
 }
-
 createGrid(userGridNumber);
+
+const gridNodeList = document.querySelectorAll('.grid');
+
+
+for (let i = 0; i < gridNodeList.length; i++) {
+    gridNodeList[i].addEventListener('mouseover', () => {
+        gridNodeList[i].classList.toggle('hover');
+    });
+}
+
+
