012b0152483ac271b7750dab5bd7276a2082ec6d
Micah Bell <micahjoshuabell@gmail.com>
2022-04-22 12:47:38 -0400

    Add reset grid functionality.  Create helper functions.

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 //
+let userGridNumber = 8;
 
-let userGridNumber = 64;
-const gridContainer = document.querySelector('#gridContainer');
+const gridContainer = document.createElement('div');
+gridContainer.setAttribute('id', 'gridContainer');
+document.body.appendChild(gridContainer);
+
 const resetButton = document.querySelector('#reset');
-
-// Set Grid size
-gridContainer.style.gridTemplateColumns = `repeat(${Math.round(Math.sqrt(userGridNumber))}, 1fr)`;
-
-// Reset the Grid
-
-
+const deleteGridButton = document.querySelector('#delete');
+//
 
 function createGrid (gridNumber) {
-    for (let i = 0; i < gridNumber; i++) {
+    for (let i = 0; i < (gridNumber * gridNumber); i++) {
         const div = document.createElement('div');
         div.classList.add('grid');
         div.classList.add('hover');
         div.classList.toggle('hover');
         gridContainer.appendChild(div);
     }
-}
+    assignClasses();
+    setStyle();
+};
+
+// Set Grid size
+function setStyle () {
+    gridContainer.style.gridTemplateColumns = `repeat(${userGridNumber}, 1fr)`;
+};
+
+function assignClasses () {
+    const gridNodeList = document.querySelectorAll('.grid');
+    for (let i = 0; i < gridNodeList.length; i++) {
+        gridNodeList[i].addEventListener('mouseover', () => {
+            gridNodeList[i].classList.toggle('hover');
+        });
+    };
+};
+
+// Delete the Grid
+    resetButton.addEventListener('click', () => {
+    let children = gridContainer.childNodes;
+    for (i = children.length; i > 0; i--) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+});
+
+// Reset the Grid
+resetButton.addEventListener('click', () => {
+    userGridNumber = parseInt(prompt('Please enter a new Grid Size'));
+    createGrid(userGridNumber);
+});
+
 createGrid(userGridNumber);
 
-const gridNodeList = document.querySelectorAll('.grid');
-
-
-for (let i = 0; i < gridNodeList.length; i++) {
-    gridNodeList[i].addEventListener('mouseover', () => {
-        gridNodeList[i].classList.toggle('hover');
-    });
-}
 
 
